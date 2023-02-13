c30fec70e79d40c25b040f2264031e988cf9d813
Sam <sjdefran@iastate.edu>
2023-02-13 15:40:47 -0600

    bot queries gpt

--- discordBot.py
+++ discordBot.py
@@ -2,12 +2,15 @@
 import os
 
 import discord
+import openai
 from dotenv import load_dotenv
 
 load_dotenv()
 DISCORD_TOKEN = os.getenv('DISCORD_TOKEN')
 DISCORD_GUILD = os.getenv('DISCORD_GUILD')
-OPENAI_TOKEN = os.getenv('OPENAI_API_KEY')
+
+
+openai.api_key = os.getenv('OPENAI_API_KEY')
 
 intents = discord.Intents.default()
 intents.message_content = True
@@ -23,23 +26,23 @@ async def on_ready():
 async def on_message(message):
     if message.author == client.user:
         return
+    
+    # GPT COMMAND
+    if message.content[0:4] == '/GPT':   
+        #save message sent from discord as prompt
+        prompt = message.content.split('/GPT ')[1]
 
-    # brooklyn_99_quotes = [
-    #     'I\'m the human form of the  emoji.',
-    #     'Bingpot!',
-    #     (
-    #         'Cool. Cool cool cool cool cool cool cool, '
-    #         'no doubt no doubt no doubt no doubt.'
-    #     ),
-    # ]
-
-    # if message.content == '99!':
-    #     response = random.choice(brooklyn_99_quotes)
-    #     await message.channel.send(response)
-
-
-    await message.channel.send(message.content)
-
+        # query gpt 
+        response = openai.Completion.create(
+            engine="text-davinci-002",
+            prompt= prompt,
+            temperature=0.5,
+            max_tokens=256,
+            top_p=1.0,
+            frequency_penalty=0.0,
+            presence_penalty=0.0
+        )
+        await message.channel.send(response['choices'][0]['text'])
 
 client.run(DISCORD_TOKEN)
 
