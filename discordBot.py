0baeb7d5dc9213f1c2df168f6176251e4479e1e3
Sam <sjdefran@iastate.edu>
2023-02-13 15:28:55 -0600

    bot echoing messages from discord server

--- discordBot.py
+++ discordBot.py
@@ -5,13 +5,41 @@ import discord
 from dotenv import load_dotenv
 
 load_dotenv()
-TOKEN = os.getenv('DISCORD_TOKEN')
+DISCORD_TOKEN = os.getenv('DISCORD_TOKEN')
+DISCORD_GUILD = os.getenv('DISCORD_GUILD')
+OPENAI_TOKEN = os.getenv('OPENAI_API_KEY')
 
-client = discord.Client(intents=discord.Intents.default())
+intents = discord.Intents.default()
+intents.message_content = True
+
+client = discord.Client(intents=intents)
 
 @client.event
 async def on_ready():
     print(f'{client.user} has connected to Discord!')
 
-client.run(TOKEN)
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    # brooklyn_99_quotes = [
+    #     'I\'m the human form of the  emoji.',
+    #     'Bingpot!',
+    #     (
+    #         'Cool. Cool cool cool cool cool cool cool, '
+    #         'no doubt no doubt no doubt no doubt.'
+    #     ),
+    # ]
+
+    # if message.content == '99!':
+    #     response = random.choice(brooklyn_99_quotes)
+    #     await message.channel.send(response)
+
+
+    await message.channel.send(message.content)
+
+
+client.run(DISCORD_TOKEN)
 
